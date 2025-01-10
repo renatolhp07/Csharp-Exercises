046d7fdee2f3d05b6c46c47825a7d5efa1d01f63
Renato Vieira Santos <renato.vieiralhp@gmail.com>
2025-01-09 23:44:47 -0300

    Programa que calcula a área de um retângulo.

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cálculo de área do retângulo: \n===========================================");
+            Console.WriteLine("\nDigite o comprimento da base do retângulo em centímetros: ");
+            int baseRetangulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nDigite a altura do retângulo em centímetros: ");
+            int alturaRetangulo = Convert.ToInt32(Console.ReadLine());
+            Retangulo r = new Retangulo();
+            r.BaseRet = baseRetangulo;
+            r.AlturaRet = alturaRetangulo;
+            r.AreaDoRetangulo(baseRetangulo, alturaRetangulo);
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+		private int baseRet;
+
+		public int BaseRet
+		{
+			get { return baseRet; }
+			set { this.baseRet = value; }
+		}
+
+		private int alturaRet;
+
+		public int AlturaRet
+		{
+			get { return alturaRet; }
+			set { this.alturaRet = value; }
+		}
+		
+		public void AreaDoRetangulo(int retBase, int retAltura)
+		{
+			retBase = this.baseRet;
+			retAltura = this.alturaRet;
+
+			int areaRet = retBase * retAltura;
+			Console.WriteLine("\nA área do retângulo é de: " + areaRet + " cm");
+			Console.ReadKey();
+        }
+    }
+}
